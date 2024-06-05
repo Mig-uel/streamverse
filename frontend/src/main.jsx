68bcbf26f006dec85db0c9e39d9237ed08359992
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-05 19:35:25 -0400

    chore: Update main.jsx to use react-router-dom for routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
+// UI Elements
 import App from './App.jsx'
-import './index.css'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index path='/' element={<App />}></Route>)
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
