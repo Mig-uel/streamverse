ba72bc67f9dd67daa16e41bec2218df3f297721f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-05 22:47:20 -0400

    chore: Update App.jsx to include header and body components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Outlet } from 'react-router-dom'
+
+// UI Elements
+import { GlobalStyles } from './GlobalStyles'
+import Header from './components/header/header.component'
+import Body from './components/body/body.component'
+
 function App() {
-  return <></>
+  return (
+    <>
+      <Header />
+      <Body />
+      <GlobalStyles />
+    </>
+  )
 }
 
 export default App
